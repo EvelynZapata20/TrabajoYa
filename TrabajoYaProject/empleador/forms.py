d3f686d39eb2a4557d861912fea31f44db01348c
JulianAgudelo12 <agudelojulian4@gmail.com>
2023-11-22 13:22:45 -0500

    Redireccion Whattsap

--- TrabajoYaProject/empleador/forms.py
+++ TrabajoYaProject/empleador/forms.py
@@ -8,3 +8,18 @@ class FormularioVacante(forms.ModelForm):
         widgets = {
             'disponibilidad': forms.CheckboxInput(attrs={'class': 'check-box'}),
         }
+
+class AppointmentForm(forms.ModelForm):
+    class Meta:
+        model= Contrato
+        fields= ['restricciones', 'tipo', 'servicio', 'duracion','condiciones','pago','fecha_inicio']
+        exclude = ['empleador_id','trabajador_id']
+        widgets = {
+            'restricciones': forms.Textarea(),
+            'tipo': forms.Textarea(),
+            'servicio': forms.Textarea(),
+            'duracion': forms.Textarea(),
+            'condiciones': forms.Textarea(),
+            'pago': forms.Textarea(),
+            'fecha_inicio': forms.DateInput(attrs={'placeholder': 'YYYY-MM-DD', 'type': 'date', 'max': '2030-01-01', 'min': '2000-01-01'}),
+        }
--- TrabajoYaProject/empleador/models.py
+++ TrabajoYaProject/empleador/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from cuenta.models import Empleador
+from cuenta.models import Empleador, Trabajador
 
 # Create your models here.
 
@@ -14,4 +14,17 @@ class VacanteLaboral(models.Model):
     oferta = models.CharField(blank= False, max_length=200)
     empleador = models.ForeignKey(Empleador, on_delete=models.CASCADE,null=True, blank=True)
     disponibilidad = models.BooleanField(default=False)
+
+
+class Contrato(models.Model):
+    id = models.AutoField(primary_key= True)
+    restricciones = models.CharField(blank= False, max_length=500)
+    tipo = models.CharField(blank= False, max_length=500)
+    servicio = models.CharField(blank= False, max_length=500)
+    duracion = models.CharField(blank= False, max_length=500)
+    condiciones = models.CharField(blank= False, max_length=500)
+    pago = models.FloatField(blank= False)
+    fecha_inicio = models.DateField(blank=False)
+    empleador_id = models.ForeignKey(Empleador, on_delete=models.CASCADE,blank=False)
+    trabajador_id = models.ForeignKey(Trabajador, on_delete=models.CASCADE,blank=False)
     
